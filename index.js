f81287a457386f8386adc757992a0f01992fa972
Daniel Cousens <github@dcousens.com>
2015-06-03 13:21:56 +1000

    index: server doesn't need to be stored

--- index.js
+++ index.js
@@ -99,12 +99,10 @@ module.exports = function (gulp) {
 	gulp.task('serve', function () {
 		var express = require('express');
 		var app = express();
+		var port = process.env.PORT || 8000
 
 		app.use(express.static('./www'));
-
-		var server = app.listen(process.env.PORT || 8000, function () {
-			console.log('Local Server ready on port %d', server.address().port);
-		});
+		app.listen(process.env.PORT || 8000, function () { console.log('Local Server ready on port %d', port); });
 	});
 
 	// Development
